18f9b780637f2dc31c42dc3cef78c5541eac55bb
shaluka135 <shalukarathnayake18@gmail.com>
2025-04-27 08:37:21 +0530

    Delete CommentRepository.java

--- server/src/main/java/com/snappix/server/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.snappix.server.repository;
-
-import com.snappix.server.model.Comment;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface CommentRepository extends MongoRepository<Comment, String> {
-    List<Comment> findByPostId(String postId);
-}
